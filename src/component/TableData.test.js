82829e850006ac5f64130012a9edce1ae84c328a
thpuloku <thanujalokuge2011@gmail.com>
2022-06-26 15:25:54 +0530

    fixed warning message from tabledata

--- src/component/TableData.test.js
+++ src/component/TableData.test.js
@@ -30,30 +30,29 @@ const testData = [
 
 describe("<TableData />", () => {
     it("renders without errors", () => {
-        render(<TableData  />);
+        render(<table><tbody><tr><TableData  /></tr></tbody></table>);
     });
     afterEach(cleanup);
 
     it("should return all good for status according to the pass variable", () => {
         const {getByTestId } = render(
-            <TableData
-                storeTemp={testData[0].storeTemp}
-                minTemp={testData[0].minTemp}
-                maxTemp={testData[0].mAXTemp}
-                statusInfo={testData[0].statusInfo}
-            />
+           <table><tbody><tr><TableData
+               storeTemp={testData[0].storeTemp}
+               minTemp={testData[0].minTemp}
+               maxTemp={testData[0].mAXTemp}
+               statusInfo={testData[0].statusInfo}
+               /></tr></tbody></table>
         );
 
         expect(getByTestId("test-span").textContent).toEqual("all good");
     });
     it("should return too low for status according to the pass variable", () => {
         const { getByTestId } = render(
-            <TableData
+            <table><tbody><tr><TableData
                 storeTemp={testData[1].storeTemp}
                 minTemp={testData[1].minTemp}
                 maxTemp={testData[1].mAXTemp}
-                statusInfo={testData[1].statusInfo}
-            />
+                statusInfo={testData[1].statusInfo} /></tr></tbody></table>
         );
 
         expect(getByTestId("test-span").textContent).toEqual("too low");
@@ -61,12 +60,12 @@ describe("<TableData />", () => {
 
     it("should return too high for status according to the pass variable", () => {
         const { getByTestId } = render(
-            <TableData
+            <table><tbody><tr><TableData
                 storeTemp={testData[2].storeTemp}
                 minTemp={testData[2].minTemp}
                 maxTemp={testData[2].mAXTemp}
                 statusInfo={testData[2].statusInfo}
-            />
+            /></tr></tbody></table>
         );
 
         expect(getByTestId("test-span").textContent).toEqual("too high");
@@ -74,12 +73,12 @@ describe("<TableData />", () => {
 
     it("should return too low for status according to the pass variable", () => {
         const { container, getByTestId } = render(
-            <TableData
+            <table><tbody><tr><TableData
                 storeTemp={testData[3].storeTemp}
                 minTemp={testData[3].minTemp}
                 maxTemp={testData[3].mAXTemp}
                 statusInfo={testData[3].statusInfo}
-            />
+            /></tr></tbody></table>
         );
 
         expect(getByTestId("test-span").textContent).toEqual("too low");
